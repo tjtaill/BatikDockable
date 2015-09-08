a18c49b6854aa9af74dcb002eb71eda5f39c7f43
troy <tjtaill36@gmail.com>
2015-09-08 02:29:37 -0400

    Finally made the auto pan with svg work. Need to work on my presentation now.

--- trunk/src/batikdockable/BatikDockablePlugin.java
+++ trunk/src/batikdockable/BatikDockablePlugin.java
@@ -1,11 +1,24 @@
 package batikdockable;
 
 import org.gjt.sp.jedit.EditPlugin;
+import org.gjt.sp.jedit.jEdit;
 
 public class BatikDockablePlugin extends EditPlugin {
+	private static final String SYNC_SVG_WITH_BUFFER_PROP = "plugin.batikdockable.syncsvg";
+
+
     public void start() {
 	}
 
 	public void stop() {
+	}
+
+	public static  void toggleSyncSvgWithBuffer() {
+		boolean status = jEdit.getBooleanProperty(SYNC_SVG_WITH_BUFFER_PROP);
+		jEdit.setBooleanProperty(SYNC_SVG_WITH_BUFFER_PROP, !status);
+	}
+
+	public static boolean isSyncSvgWithBuffer() {
+		return jEdit.getBooleanProperty(SYNC_SVG_WITH_BUFFER_PROP);
 	}
 }
